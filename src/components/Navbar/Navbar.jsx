e26771c5b62dd1e22d0c7ff05525d7b66b6e3f0f
Delgorithm <delgorithm@gmail.com>
2023-12-26 16:49:38 +0100

    feature : responsive md

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-neutrals flex justify-between items-center bg-black-2 p-6 pt-10 md:'>
+    <div className='bg-neutrals flex justify-between items-center bg-black-2 p-6 pt-10 md:pt-4'>
       <h1 id='accueil' className='text-2xl text-white md:text-xl'>Reviewent</h1>
       <div>
         <ul className='text-white md:text-sm'>
-          <li className='font-light flex gap-10 border-[1px] border-neutrals-11 p-2 px-10 ml-16 rounded-xl md:ml-0 md:px-6'>
+          <li className='font-light flex gap-10 border-[1px] border-neutrals-11 p-2 px-10 ml-16 rounded-xl md:ml-0 md:px-3 md:gap-4'>
             <a href="#features" className='p-2 hover:text-washed-purple-600 hover:-translate-y-1 transform transition duration-150'>Features</a>
             <a href="#testimonials" className='p-2 hover:text-washed-purple-600 hover:-translate-y-1 transform transition duration-150'>Testimonials</a>
             <a href="#pricing" className='p-2 hover:text-washed-purple-600 hover:-translate-y-1 transform transition duration-150'>Pricing</a>
